5a2194bcf55065cea89fc601680d56d3d8a4c912
Davis Van Sant <davisvansant@users.noreply.github.com>
2019-06-25 14:45:39 -0700

    all the goodies found in chapter 14 for crates.io

---
+++ 14/add/add-one/src/lib.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
